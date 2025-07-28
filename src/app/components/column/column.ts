6c713aae44d14b7cdcd3fef0749a963fd0f51fe4
vagnerxpert <vagner@xpert.com.br>
2025-07-28 11:29:50 -0300

    feat(components): created the components Board, Column, and Task

---
+++ src/app/components/column/column.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-column',
+  imports: [],
+  templateUrl: './column.html',
+  styleUrl: './column.scss'
+})
+export class Column {
+
+}
